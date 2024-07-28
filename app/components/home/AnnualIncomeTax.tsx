5ea18e4562ab901bb8fb6be6a9bd12602e914dd0
danishyarkhwork <danishyarkhwork@gmail.com>
2024-07-28 20:05:48 +0430

    add: the Skeleton functionality added to AnnualIncomeTax

--- app/components/home/AnnualIncomeTax.tsx
+++ app/components/home/AnnualIncomeTax.tsx
@@ -4,14 +4,23 @@ import { FullScreen, useFullScreenHandle } from "react-full-screen";
 import { NumericFormat } from "react-number-format";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import Skeleton from "../common/Skeleton";
 
 const AnnualIncomeTax: React.FC = () => {
   const [netIncome, setNetIncome] = useState<number>(0);
   const [legalPersonsTax, setLegalPersonsTax] = useState<number>(0);
   const [naturalPersonsTax, setNaturalPersonsTax] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(true);
   const handle = useFullScreenHandle();
 
   const calculateLegalPersonsTax = (income: number) => income * 0.2;
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
 
   const calculateNaturalPersonsTax = (income: number) => {
     if (income <= 60000) {
@@ -94,116 +103,130 @@ const AnnualIncomeTax: React.FC = () => {
           className="container mx-auto max-w-6xl bg-gray-100 p-2 rounded-lg"
           id="annual-income-tax-content"
         >
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div>
-              <div className="mb-4">
-                <label
-                  htmlFor="netIncome"
-                  className="block text-lg font-medium text-gray-700"
-                >
-                  What is the annual net income?
-                </label>
-                <NumericFormat
-                  thousandSeparator={true}
-                  value={netIncome}
-                  onValueChange={(values) =>
-                    setNetIncome(values.floatValue || 0)
-                  }
-                  className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-xl"
-                  placeholder="AFN 0"
-                  isAllowed={({ floatValue }) =>
-                    floatValue === undefined ||
-                    floatValue <= Number.MAX_SAFE_INTEGER
-                  }
-                />
+          {loading ? (
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div>
+                <Skeleton className="h-10 mb-4" />
+                <Skeleton className="h-10 mb-4" />
+                <Skeleton className="h-10 mb-4" />
               </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="legalPersonsTax"
-                  className="block text-lg font-medium text-gray-700"
-                >
-                  Legal Persons
-                </label>
-                <NumericFormat
-                  thousandSeparator={true}
-                  value={legalPersonsTax}
-                  readOnly
-                  className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-xl bg-gray-200"
-                  placeholder="AFN 0"
-                />
-              </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="naturalPersonsTax"
-                  className="block text-lg font-medium text-gray-700"
-                >
-                  Natural Persons
-                </label>
-                <NumericFormat
-                  thousandSeparator={true}
-                  value={naturalPersonsTax}
-                  readOnly
-                  className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-xl bg-gray-200"
-                  placeholder="AFN 0"
-                />
+              <div>
+                <Skeleton className="h-10 mb-4" />
+                <Skeleton className="h-40 mb-4" />
               </div>
             </div>
-            <div>
-              <h3 className="text-xl font-semibold mb-4">
-                A Summary of Annual Income Tax
-              </h3>
-              <table className="w-full table-auto mb-4">
-                <thead>
-                  <tr className="bg-gray-200">
-                    <th className="px-4 py-2">Entity Type</th>
-                    <th className="px-4 py-2">Net Income</th>
-                    <th className="px-4 py-2">Tax Rate</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td className="border px-4 py-2" rowSpan={2}>
-                      Legal Persons
-                    </td>
-                    <td className="border px-4 py-2">Over 0AFN</td>
-                    <td className="border px-4 py-2">20% flat</td>
-                  </tr>
-                  <tr>
-                    <td className="border px-4 py-2" colSpan={2}></td>
-                  </tr>
-                  <tr className="bg-gray-100">
-                    <td className="border px-4 py-2" rowSpan={4}>
-                      Natural Persons
-                    </td>
-                    <td className="border px-4 py-2">0 to 60,000AFN</td>
-                    <td className="border px-4 py-2">0%</td>
-                  </tr>
-                  <tr className="bg-gray-100">
-                    <td className="border px-4 py-2">
-                      60,000AFN to 150,000AFN
-                    </td>
-                    <td className="border px-4 py-2">2% over 60,000AFN</td>
-                  </tr>
-                  <tr className="bg-gray-100">
-                    <td className="border px-4 py-2">
-                      150,000AFN to 1,200,000AFN
-                    </td>
-                    <td className="border px-4 py-2">
-                      1,800AFN + 10% over 150,000AFN
-                    </td>
-                  </tr>
-                  <tr className="bg-gray-100">
-                    <td className="border px-4 py-2">Over 1,200,000AFN</td>
-                    <td className="border px-4 py-2">
-                      106,800AFN + 20% over 1,200,000AFN
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <p className="font-semibold">Payment Due Date:</p>
-              <p>Within 3 months of the end of the Afghan fiscal year.</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="netIncome"
+                    className="block text-lg font-medium text-gray-700"
+                  >
+                    What is the annual net income?
+                  </label>
+                  <NumericFormat
+                    thousandSeparator={true}
+                    value={netIncome}
+                    onValueChange={(values) =>
+                      setNetIncome(values.floatValue || 0)
+                    }
+                    className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-xl"
+                    placeholder="AFN 0"
+                    isAllowed={({ floatValue }) =>
+                      floatValue === undefined ||
+                      floatValue <= Number.MAX_SAFE_INTEGER
+                    }
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="legalPersonsTax"
+                    className="block text-lg font-medium text-gray-700"
+                  >
+                    Legal Persons
+                  </label>
+                  <NumericFormat
+                    thousandSeparator={true}
+                    value={legalPersonsTax}
+                    readOnly
+                    className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-xl bg-gray-200"
+                    placeholder="AFN 0"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="naturalPersonsTax"
+                    className="block text-lg font-medium text-gray-700"
+                  >
+                    Natural Persons
+                  </label>
+                  <NumericFormat
+                    thousandSeparator={true}
+                    value={naturalPersonsTax}
+                    readOnly
+                    className="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-xl bg-gray-200"
+                    placeholder="AFN 0"
+                  />
+                </div>
+              </div>
+              <div>
+                <h3 className="text-xl font-semibold mb-4">
+                  A Summary of Annual Income Tax
+                </h3>
+                <table className="w-full table-auto mb-4">
+                  <thead>
+                    <tr className="bg-gray-200">
+                      <th className="px-4 py-2">Entity Type</th>
+                      <th className="px-4 py-2">Net Income</th>
+                      <th className="px-4 py-2">Tax Rate</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className="border px-4 py-2" rowSpan={2}>
+                        Legal Persons
+                      </td>
+                      <td className="border px-4 py-2">Over 0AFN</td>
+                      <td className="border px-4 py-2">20% flat</td>
+                    </tr>
+                    <tr>
+                      <td className="border px-4 py-2" colSpan={2}></td>
+                    </tr>
+                    <tr className="bg-gray-100">
+                      <td className="border px-4 py-2" rowSpan={4}>
+                        Natural Persons
+                      </td>
+                      <td className="border px-4 py-2">0 to 60,000AFN</td>
+                      <td className="border px-4 py-2">0%</td>
+                    </tr>
+                    <tr className="bg-gray-100">
+                      <td className="border px-4 py-2">
+                        60,000AFN to 150,000AFN
+                      </td>
+                      <td className="border px-4 py-2">2% over 60,000AFN</td>
+                    </tr>
+                    <tr className="bg-gray-100">
+                      <td className="border px-4 py-2">
+                        150,000AFN to 1,200,000AFN
+                      </td>
+                      <td className="border px-4 py-2">
+                        1,800AFN + 10% over 150,000AFN
+                      </td>
+                    </tr>
+                    <tr className="bg-gray-100">
+                      <td className="border px-4 py-2">Over 1,200,000AFN</td>
+                      <td className="border px-4 py-2">
+                        106,800AFN + 20% over 1,200,000AFN
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <p className="font-semibold">Payment Due Date:</p>
+                <p>Within 3 months of the end of the Afghan fiscal year.</p>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </FullScreen>
