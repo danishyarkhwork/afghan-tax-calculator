b1fb08ec17f80ed3e230f86962eac0c927c37211
danishyarkhwork <danishyarkhwork@gmail.com>
2024-07-30 12:43:19 +0430

    fix: footer fixed in mobile version

--- app/components/common/Footer.tsx
+++ app/components/common/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto px-4">
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center flex-col md:flex-row">
           <p className="text-sm mr-1">
             &copy; 2024{" "}
             <a
@@ -15,7 +15,9 @@ const Footer: React.FC = () => {
             </a>
             . All rights reserved.
           </p>
-          <p className="text-sm">
+          <div className="hidden md:block">
+            {" "}
+            {/* This will hide on mobile and show on medium and larger screens */}
             Powered by{" "}
             <a
               href="https://khaliddanishyar.com/"
@@ -23,7 +25,18 @@ const Footer: React.FC = () => {
             >
               Khalid Danishyar
             </a>
-          </p>
+          </div>
+          <div className="block md:hidden text-sm mt-2">
+            {" "}
+            {/* This will only be shown on mobile */}
+            Powered by{" "}
+            <a
+              href="https://khaliddanishyar.com/"
+              className="font-semibold text-teal-400 hover:text-teal-300"
+            >
+              Khalid Danishyar
+            </a>
+          </div>
         </div>
       </div>
     </footer>
