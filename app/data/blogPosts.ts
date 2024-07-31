bf38bfaa117cecf854637fc6a0be55140bf92636
danishyarkhwork <danishyarkhwork@gmail.com>
2024-07-31 18:18:27 +0430

    fix: blog post fixed

--- app/data/blogPosts.ts
+++ app/data/blogPosts.ts
@@ -1,23 +1,136 @@
 export const blogPosts = [
   {
-    slug: "first-post",
-    title: "First Post",
-    excerpt: "This is the summary of the first post.",
-    content: "This is the full content of the first post.",
+    slug: "non-profit-organization",
+    title:
+      "Confirming an exemption from income taxation of a non-profit organization.",
+    excerpt:
+      "Non profit organizations may apply to the Afghanistan Revenue Department (ARD) for a private...",
+    content: [
+      {
+        subtitle: "Introduction",
+        paragraph:
+          "Non profit organizations may apply to the Afghanistan Revenue Department (ARD) for a private ruling to qualify for and obtain tax exempt status based on a legal interpretation of their tax liability based on their operations related to the Income Tax Law 2009, Afghanistan’s laws governing non governmental organizations, and relevant treaties as may apply to the organization’s purpose and funding situation.",
+      },
+      {
+        subtitle: "Application Process",
+        paragraph:
+          "The application process involves submitting necessary documents to the ARD, which include detailed financial statements and descriptions of the organization's activities.",
+      },
+    ],
     image: "/blog-images/first.png",
+    guideLink:
+      "/taxes/Guide 23 Confirming an exemption from income taxation of a.pdf",
   },
   {
-    slug: "second-post",
-    title: "Second Post",
-    excerpt: "This is the summary of the second post.",
-    content: "This is the full content of the second post.",
+    slug: "withholding-tax-on-contractor-services",
+    title: "Withholding Tax on Contractor Services in Afghanistan",
+    excerpt:
+      "According to Article 72 of the Income Tax Law 2009, legal and natural persons who...",
+    content: [
+      {
+        subtitle: "Overview",
+        paragraph:
+          "According to Article 72 of the Income Tax Law 2009, legal and natural persons who provide supplies, materials, services and construction under contract are subject to tax withholding from the gross amount payable to the contractor.",
+      },
+      {
+        subtitle: "Exclusions",
+        paragraph:
+          "Natural persons, who earn taxable salaries according to provision of Article 17 (1) of this law, are excluded from this provision. They are treated as employees and subject to ordinary wage withholding (See Guide 05 – Wage Withholding Tax for more information).",
+      },
+    ],
     image: "/blog-images/second.png",
+    guideLink: "/taxes/Guide 21 - Withholding Tax on Contractor Services.pdf",
   },
   {
-    slug: "third-post",
-    title: "Third Post",
-    excerpt: "This is the summary of the second post.",
-    content: "This is the full content of the second post.",
+    slug: "taxpayer-identification-number-tin",
+    title: "Taxpayer Identification Number (TIN) of Afghanistan",
+    excerpt:
+      "Anyone required to pay taxes or customs duties, or anyone with a bank account, must apply...",
+    content: [
+      {
+        subtitle: "Requirement",
+        paragraph:
+          "Anyone required to pay taxes or customs duties, or anyone with a bank account, must apply for a Tax-payer Identification Number (TIN) (Article 86).",
+      },
+      {
+        subtitle: "Purpose",
+        paragraph:
+          "The purpose of the TIN is twofold: it 1) assures proper crediting of payments and 2) contributes to more efficient tax administration.",
+      },
+      {
+        subtitle: "Penalties",
+        paragraph:
+          "The Income Tax Law 2009 provides for additional taxes and penalties should a person fail to apply for a TIN without ‘reasonable cause’ (Article 105).",
+      },
+    ],
     image: "/blog-images/third.jpg",
+    guideLink: "/taxes/Guide 18 - Taxpayer Identification Number (TIN).pdf", // Example guide link
+  },
+  {
+    slug: "taxpayer-identification-number-tin",
+    title: "Taxpayer Identification Number (TIN) of Afghanistan",
+    excerpt:
+      "Anyone required to pay taxes or customs duties, or anyone with a bank account, must apply...",
+    content: [
+      {
+        subtitle: "Requirement",
+        paragraph:
+          "Anyone required to pay taxes or customs duties, or anyone with a bank account, must apply for a Tax-payer Identification Number (TIN) (Article 86).",
+      },
+      {
+        subtitle: "Purpose",
+        paragraph:
+          "The purpose of the TIN is twofold: it 1) assures proper crediting of payments and 2) contributes to more efficient tax administration.",
+      },
+      {
+        subtitle: "Penalties",
+        paragraph:
+          "The Income Tax Law 2009 provides for additional taxes and penalties should a person fail to apply for a TIN without ‘reasonable cause’ (Article 105).",
+      },
+    ],
+    image: "/blog-images/third.jpg",
+    guideLink: "/taxes/Guide 18 - Taxpayer Identification Number (TIN).pdf", // Example guide link
+  },
+  {
+    slug: "withholding-tax-on-contractor-services",
+    title: "Withholding Tax on Contractor Services in Afghanistan",
+    excerpt:
+      "According to Article 72 of the Income Tax Law 2009, legal and natural persons who...",
+    content: [
+      {
+        subtitle: "Overview",
+        paragraph:
+          "According to Article 72 of the Income Tax Law 2009, legal and natural persons who provide supplies, materials, services and construction under contract are subject to tax withholding from the gross amount payable to the contractor.",
+      },
+      {
+        subtitle: "Exclusions",
+        paragraph:
+          "Natural persons, who earn taxable salaries according to provision of Article 17 (1) of this law, are excluded from this provision. They are treated as employees and subject to ordinary wage withholding (See Guide 05 – Wage Withholding Tax for more information).",
+      },
+    ],
+    image: "/blog-images/second.png",
+    guideLink: "/taxes/Guide 21 - Withholding Tax on Contractor Services.pdf",
+  },
+  {
+    slug: "non-profit-organization",
+    title:
+      "Confirming an exemption from income taxation of a non-profit organization.",
+    excerpt:
+      "Non profit organizations may apply to the Afghanistan Revenue Department (ARD) for a private...",
+    content: [
+      {
+        subtitle: "Introduction",
+        paragraph:
+          "Non profit organizations may apply to the Afghanistan Revenue Department (ARD) for a private ruling to qualify for and obtain tax exempt status based on a legal interpretation of their tax liability based on their operations related to the Income Tax Law 2009, Afghanistan’s laws governing non governmental organizations, and relevant treaties as may apply to the organization’s purpose and funding situation.",
+      },
+      {
+        subtitle: "Application Process",
+        paragraph:
+          "The application process involves submitting necessary documents to the ARD, which include detailed financial statements and descriptions of the organization's activities.",
+      },
+    ],
+    image: "/blog-images/first.png",
+    guideLink:
+      "/taxes/Guide 23 Confirming an exemption from income taxation of a.pdf",
   },
 ];
