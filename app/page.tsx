5a00b7babc21b05dbb7fc12835b6b366d4339483
danishyarkhwork <danishyarkhwork@gmail.com>
2024-08-01 19:54:29 +0430

    add: added the Skeleton functionality to the blog posts

--- app/page.tsx
+++ app/page.tsx
@@ -111,7 +111,7 @@ const HomePage: React.FC = () => {
 
         {/* Blog Section */}
         <section className="mt-12">
-          <h3 className="text-xl md:text-3xl font-bold mb-4 text-center">
+          <h3 className="text-4xl md:text-4xl font-bold mb-4 text-center">
             Recent Blog Posts
           </h3>
           {loading ? (
